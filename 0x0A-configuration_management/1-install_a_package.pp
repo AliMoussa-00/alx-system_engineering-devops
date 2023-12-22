5a0b7204ad117565016c1d2977e84260045b91cd
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-12-22 10:30:09 -0800

    fix .1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 # installing the package flask
 
+package { 'Werkzeug':
+ensure   => '2.1.1',
+provider => 'pip3'
+}
+
 package { 'flask':
 ensure   => '2.1.0',
 provider => 'pip3'
+require  => Package['Werkzeug']
 }
