730f468da6f2e97a14d5825d16824864e405c39f
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-12-25 03:12:00 -0800

    fix .100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # puppet script to set up our 'ssh_config'
 
-file { 'set_key':
+file_line { 'set_key':
 ensure => 'present',
 path   => '/etc/ssh/ssh_config',
 line   => '	IdentityFile ~/.ssh/school'
 }
 
-file { 'no_pass':
+file_line { 'no_pass':
 ensure => 'present',
 path   => '/etc/ssh/ssh_config',
 line   => '	PasswordAuthentication no'
