4919c76c91197e4f447cd1f73e273ba4662b796c
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-01-03 10:59:48 -0800

    fixx.7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,43 +1,32 @@
-exec { 'update':
-command => '/usr/bin/sudo apt update',
-}
-
-package { 'nginx':
-ensure  => installed,
-require => Exec['update']
-}
-
-file { '/var/www/html/index.nginx-debian.html':
-ensure  => present,
-content => 'Hello World!',
-require => Package['nginx'],
-}
-
-file { '/etc/nginx/sites-available/default':
-ensure  => present,
-require => Package['nginx'],
-notify  => Service['nginx'],
-content => 'server {
+$default = "server {
         listen 80 default_server;
         listen [::]:80 default_server;
 
         root /var/www/html;
-
-        index index.html index.htm index.nginx-debian.html;
-
-        server_name _;
-
-        location / {
-                try_files \$uri \$uri/ =404;
-        }
+        index index.html;
 
         location /redirect_me {
                 return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
         }
-}'
+}"
+
+package { 'nginx':
+ensure  => 'installed',
 }
 
-service { 'nginx':
-ensure => 'running',
-enable => true,
+file { '/var/www/html/index.nginx-debian.html':
+ensure  => 'present',
+content => 'Hello World!',
+require => Package['nginx'],
+mode    => '0644'
+}
+
+file { '/etc/nginx/sites-available/default':
+ensure  => 'present',
+require => Package['nginx'],
+content => $default
+}
+
+exec { 'service nginx restart':
+path => ['/usr/sbin', '/usr/bin']
 }
