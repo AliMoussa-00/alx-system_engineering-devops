2de2f3e482de3f3a1d1df125987e6ef8b965dbb0
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-01-03 10:45:24 -0800

    .7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,49 @@
+exec { 'update':
+command => '/usr/bin/sudo apt update',
+}
+
+package { 'nginx':
+ensure  => installed,
+require => Exec['update']
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+ensure  => present,
+content => 'Hello World!',
+require => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+ensure  => present,
+require => Package['nginx'],
+notify  => Service['nginx'],
+content => 'server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+
+        location / {
+                try_files \$uri \$uri/ =404;
+        }
+
+        location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+
+        error_page 404 /404.html;
+        location = /404.html {
+                internal;
+        }
+
+}'
+}
+
+service { 'nginx':
+ensure => 'running',
+enable => true,
+}
