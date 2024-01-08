ec01c22d61ad478ce2cb27e0f1baca204a402fd9
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-01-08 12:08:34 -0800

    fix ...2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -15,9 +15,10 @@ exec {'add_header':
 environment => ["HOST=${hostname}"],
 command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
 provider    => shell,
+require     => Package['nginx'],
 }
 
-exec { 'service nginx restart':
-path    => ['/usr/sbin', '/usr/bin'],
-require => Exec['add_header'],
+exec { 'restart nginx':
+provider => shell,
+command  => 'sudo service nginx restart',
 }
