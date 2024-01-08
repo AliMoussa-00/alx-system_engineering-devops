b150b85ea6dce042eaf7db3e56ade26d12796b41
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-01-08 12:05:25 -0800

    fixx .2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,28 +1,5 @@
 # creating a custom HTTP header response, but with Puppet.
 
-$default = 'server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        root /var/www/html;
-
-        index index.html index.htm index.nginx-debian.html;
-
-        server_name _;
-
-        location / {
-                try_files \$uri \$uri/ =404;
-        }
-        location /redirect_me {
-                return 301 https:#www.youtube.com/watch?v=QH2-TGUlwu4;
-        }
-
-        error_page 404 /404.html;
-        location = /404.html {
-                internal;
-        }
-}'
-
 exec {'update':
   provider => shell,
   command  => 'sudo apt-get -y update',
@@ -33,35 +10,11 @@ ensure  => 'installed',
 require => Exec['update'],
 }
 
-file { 'index':
-ensure  => 'present',
-path    => '/var/www/html/index.nginx-debian.html',
-content => 'Hello World!',
-mode    => '0644',
-require => Package['nginx'],
-}
-
-file { 'error_404':
-ensure  => 'present',
-path    => '/var/www/html/404.html',
-content => 'Ceci n\'est pas une page',
-mode    => '0644',
-require => Package['nginx'],
-}
-
-file { 'server_config':
-ensure  => 'present',
-path    => '/etc/nginx/sites-available/default',
-content => $default,
-require => Package['nginx'],
-}
-
 # adding header
 exec {'add_header':
 environment => ["HOST=${hostname}"],
-command     => 'sudo sed -i "/root \/var\/www\/html;/a $(printf "\t")add_header X-Served-By $HOST;" /etc/nginx/sites-available/default',
+command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
 provider    => shell,
-require     => File['server_config'],
 }
 
 exec { 'service nginx restart':
