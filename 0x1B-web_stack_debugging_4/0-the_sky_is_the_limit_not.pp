5e5d23f2e4282bfd03a572f95cf0af36d825338d
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-02-22 14:01:33 +0000

    fix 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,7 @@
 # will increase the number of the request the server can handle
 # which will reduce the number of failed requests
 
-exec { 'fix--for-nginx':
+exec { 'update_nginx_conf':
   command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
   path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
