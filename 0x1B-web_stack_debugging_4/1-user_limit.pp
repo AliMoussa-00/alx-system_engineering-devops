016b48b5be69e039f3407a2c5a734d2f36e488b6
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-02-22 14:35:03 +0000

    fix .1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,7 @@
 # fix the 'Too many open files'
 # It indicates that there are too many open files to open this file successfully.
 
-exec {'change-os-configuration-for-holberton-user':
+exec {'replace-the-limits-on-holberton-user':
   command  => 'sed -i s/holberton/foo/ /etc/security/limits.conf',
   provider => 'shell'
 }
